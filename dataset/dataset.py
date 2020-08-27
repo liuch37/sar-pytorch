d97c70f16c3721f5a90d173b48200fc3d1374071
Chun-Hao Liu <chun-hao.liu@ericsson.com>
2020-08-27 10:28:07 -0700

    support iiit5k dataset

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -8,6 +8,8 @@ import os
 import torch
 import numpy as np
 import xml.etree.ElementTree as ET
+from scipy.io import loadmat
+import pdb
 
 def dictionary_generator(END='END', PADDING='PAD', UNKNOWN='UNK'):
     '''
@@ -27,14 +29,13 @@ def dictionary_generator(END='END', PADDING='PAD', UNKNOWN='UNK'):
 
     return voc, char2id, id2char
 
-def svt_xml_extractor(img_path, label_path):
+def svt_xml_extractor(label_path):
     '''
     This code is to extract xml labels from SVT dataset
     Input:
-    img_path: path for all image folder
     label_path: xml label path file
     Output:
-    dict_img: [image_name, bounding box, labels, lexicon]]
+    dict_img: [image_name, bounding box, labels, lexicon]
     '''
     # create element tree object
     tree = ET.parse(label_path)
@@ -61,16 +62,31 @@ def svt_xml_extractor(img_path, label_path):
 
     return dict_img
 
-def iiit5k_mat_extractor(img_path, label_path):
+def iiit5k_mat_extractor(label_path):
     '''
     This code is to extract mat labels from IIIT5k dataset
     Input:
-    img_path: path for all image folder
     label_path: mat label path file
     Output:
-    dict_img: [image_name, labels, lexicon]]
+    dict_img: [image_name, labels, small_lexicon, medium_lexicon]
     '''
-    pass
+    # create empty list for news items
+    dict_img = []
+
+    mat_contents = loadmat(label_path)
+
+    if 'traindata' in mat_contents:
+        key = 'traindata'
+    else:
+        key = 'testdata'
+    for i in range(len(mat_contents[key][0])):
+        name = mat_contents[key][0][i][0][0]
+        label = mat_contents[key][0][i][1][0]
+        small_lexi = [item[0] for item in mat_contents[key][0][i][2][0]]
+        medium_lexi = [item[0] for item in mat_contents[key][0][i][3][0]]
+        dict_img.append([name, label, small_lexi, medium_lexi])
+
+    return dict_img
 
 class svt_dataset_builder(data.Dataset):
     def __init__(self, height, width, seq_len, total_img_path, xml_path):
@@ -86,7 +102,7 @@ class svt_dataset_builder(data.Dataset):
         self.height = height
         self.width = width
         self.seq_len = seq_len
-        self.dictionary = svt_xml_extractor(total_img_path, xml_path)
+        self.dictionary = svt_xml_extractor(xml_path)
         self.total_img_name = os.listdir(total_img_path)
         self.dataset = []
         self.voc, self.char2id, _ = dictionary_generator()
@@ -142,14 +158,34 @@ class iiit5k_dataset_builder(data.Dataset):
         self.height = height
         self.width = width
         self.seq_len = seq_len
-        self.dictionary = iiit5k_mat_extractor(total_img_path, annotation_path)
+        self.dictionary = iiit5k_mat_extractor(annotation_path)
         self.total_img_name = os.listdir(total_img_path)
         self.dataset = []
         self.voc, self.char2id, _ = dictionary_generator()
         self.output_classes = len(self.voc)
 
+        for items in self.dictionary:
+            if items[0].split('/')[-1] in self.total_img_name:
+                self.dataset.append([items[0].split('/')[-1],items[1],items[2],items[3]])
+
     def __getitem__(self, index):
-        pass
+        img_name, label, small_lex, medium_lex = self.dataset[index]
+        IMG = cv2.imread(os.path.join(self.total_img_path,img_name))
+        IMG = cv2.resize(IMG, (self.width, self.height)) # resize
+        IMG = (IMG - 127.5)/127.5 # normalization to [-1,1]
+        IMG = torch.FloatTensor(IMG) # convert to tensor [H, W, C]
+        IMG = IMG.permute(2,0,1) # [C, H, W]
+        y_true = np.ones(self.seq_len)*self.char2id['PAD'] # initialize y_true with 'PAD', size [seq_len]
+        # label processing
+        for i, c in enumerate(label):
+            index = self.char2id[c]
+            y_true[i] = index
+        y_true[-1] = self.char2id['END'] # always put 'END' in the end
+        y_true = y_true.astype(int) # must to integer index for one-hot encoding
+        # convert to one-hot encoding
+        y_onehot = np.eye(self.output_classes)[y_true] # [seq_len, output_classes]
+
+        return IMG, torch.FloatTensor(y_onehot)
 
     def __len__(self):
         return len(self.dataset)
@@ -185,11 +221,15 @@ if __name__ == '__main__':
     img_path = '../svt/img/'
     train_xml_path = '../svt/train.xml'
     test_xml_path = '../svt/test.xml'
+
+    img_path_iiit = '../IIIT5K/train/'
+    annotation_path_iiit = '../IIIT5K/traindata.mat'
+
     height = 48 # input height pixel
     width = 64 # input width pixel
     seq_len = 40 # sequence length
 
-    train_dict = svt_xml_extractor(img_path, train_xml_path)
+    train_dict = svt_xml_extractor(train_xml_path)
     print("Dictionary for training set is:", train_dict)
 
     train_dataset = svt_dataset_builder(height, width, seq_len, img_path, train_xml_path)
@@ -199,9 +239,21 @@ if __name__ == '__main__':
 
     test_dataset = svt_dataset_builder(height, width, seq_len, img_path, test_xml_path)
 
+    train_dict_iiit = iiit5k_mat_extractor(annotation_path_iiit)
+    print("Dictionary for training set is:", train_dict_iiit)
+
+    train_dataset_iiit5k = iiit5k_dataset_builder(height, width, seq_len, img_path_iiit, annotation_path_iiit)
+
     for i, item in enumerate(test_dataset):
         print(item[0].shape,item[1].shape)
         IMG = item[0].permute(1,2,0)
         IMG = IMG.detach().numpy()
         IMG = (IMG*127.5+127.5).astype(np.uint8)
-        cv2.imwrite('../test/'+str(i)+'.jpg', IMG)
+        cv2.imwrite('../test/svt_'+str(i)+'.jpg', IMG)
+
+    for i, item in enumerate(train_dataset_iiit5k):
+        print(item[0].shape,item[1].shape)
+        IMG = item[0].permute(1,2,0)
+        IMG = IMG.detach().numpy()
+        IMG = (IMG*127.5+127.5).astype(np.uint8)
+        cv2.imwrite('../test/iiit_'+str(i)+'.jpg', IMG)
